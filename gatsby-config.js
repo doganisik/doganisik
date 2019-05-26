99861bd44624bbf36dc6b4cb1ab593129b396c52
Emre Zeytin <emrzeytin@gmail.com>
2019-05-26 14:29:14 +0300

    post 1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Doğan Işık Köşe Yazıları`,
+    author: `Doğan Işık`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `doganisik35`,
     },
   },
   plugins: [
